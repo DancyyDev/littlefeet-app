d79440547cfdaf837f376e9f62a2aa01b07942c3
Daniel Cha <cha.danieldev@gmail.com>
2023-01-09 23:48:28 -0500

    working with the input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Form() {
+
+  const [totalDonation, setTotalDonation] = useState(0)
+  const [donation, setDonation] = useState('')
+
+  let addDonation = (e) => {
+    e.preventDefault()
+    setTotalDonation((prevState) =>{
+      return prevState + parseInt(donation)
+    })
+    setDonation('')
+  }
+  
   return (
     <div>
-        <p>
-          Potato
-        </p>
+
+        <h4>
+          {totalDonation}
+        </h4>
+
+        <form>
+          <input type="number" className="moneyInput" value={donation} onChange={(e) =>setDonation(e.target.value)} />
+          <button onClick={ addDonation }>Donate</button>
+        </form>
+
     </div>
   )
 }
